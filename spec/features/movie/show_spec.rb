0239ae4190f1d6ea5dbb6a3f0f9dfad00b78b851
Joey Haas <josephthaas@gmail.com>
2021-04-12 10:29:44 -0600

    completed user story3

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'studio show page' do
+RSpec.describe 'movie show page' do
   before(:each) do
     @studio_1 = Studio.create!(name: "Joey studio", location: "Denver")
     @movie_1= @studio_1.movies.create!(
@@ -8,33 +8,27 @@ RSpec.describe 'studio show page' do
       creation_year: 1999,
       genre: "sci-fi"
     )
-    @movie_2 = @studio_1.movies.create!(
-      title: "haunting on the hill",
-      creation_year: 1980,
-      genre: "horror"
-    )
-    @movie_3 = @studio_1.movies.create!(
-      title: "happy gilmore",
-      creation_year: 1995,
-      genre: "comedy"
-    )
+    @actor_1= Actor.create!(name: "Harrison", age: 55, currently_working: true)
+    @actor_2= Actor.create!(name: "Ford", age: 38, currently_working: false)
+    ActorMovie.create!(actor: @actor_1, movie: @movie_1)
+    ActorMovie.create!(actor: @actor_2, movie: @movie_1)
+
   end
   it 'lists all the fields ' do
 
-    visit  "/studios/#{@studio_1.id}"
+    visit  "/movies/#{@movie_1.id}"
 
-    expect(page).to have_content(@studio_1.name)
-    expect(page).to have_content(@studio_1.location)
-    within "#movie-#{@movie_1.id}" do
-      expect(page).to have_content(@movie_1.title)
+    expect(page).to have_content(@movie_1.title)
+    expect(page).to have_content(@movie_1.creation_year)
+    expect(page).to have_content(@movie_1.genre)
+    
+    within "#actor-#{@actor_1.id}" do
+      expect(page).to have_content(@actor_1.name)
     end
 
-    within "#movie-#{@movie_2.id}" do
-      expect(page).to have_content(@movie_2.title)
+    within "#actor-#{@actor_2.id}" do
+      expect(page).to have_content(@actor_2.name)
     end
 
-    within "#movie-#{@movie_3.id}" do
-      expect(page).to have_content(@movie_3.title)
-    end
   end
 end
