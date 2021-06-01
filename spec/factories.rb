b97d35ef9fe0a5bf69cdf57d8363f8518aff3502
Richard <rdesilvey@gmail.com>
2021-06-01 09:28:01 -0600

    Build factories for test data

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,12 @@
 FactoryBot.define do
-    
+  factory :movie do
+    title { Faker::Movie.title }
+    creation_year { rand(1999..2021) }
+    genre { Faker::Book.genre }
+  end
+
+  factory :studio do
+    name { Faker::Company.name }
+    location  {Faker::Address.city }
+  end
 end
