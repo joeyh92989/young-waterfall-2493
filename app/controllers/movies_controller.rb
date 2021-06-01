dfb10804bb5ffde063ed170e29280f0b369ac388
Richard <rdesilvey@gmail.com>
2021-06-01 11:37:45 -0600

    Implement user story 4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,14 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show ]
+  before_action :set_movie, only: %i[ show add_actor ]
 
   def show
+  end
+
+  def add_actor
+    actor = Actor.where(name: params[:actor_name]).limit(1).first
+    @movie.actors << actor if not(actor.nil?)
+
+    redirect_to movie_path(@movie)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :studios, only: [ :show ] do
     resources :movies, only: [ :show ]
   end
+
+  resources :movies, only: [ :show ]
+
+  post '/movies/:id/actors', to: 'movies#add_actor'
 end
