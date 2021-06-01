25ca3f76db7fb0f1faf8df34dcef16e10abe47b5
Richard <rdesilvey@gmail.com>
2021-06-01 10:01:01 -0600

    Setup tests for story 2

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe 'The show page for a studio,' do
     @movie_1 = FactoryBot.create(:movie, studio: @studio)
     @movie_2 = FactoryBot.create(:movie, studio: @studio)
     @movie_3 = FactoryBot.create(:movie, studio: @studio)
+
+    @actor_1 = FactoryBot.create(:actor, currently_working: false)
+    @actor_2 = FactoryBot.create(:actor)
+    @actor_3 = FactoryBot.create(:actor)
+    @actor_4 = FactoryBot.create(:actor)
+
+    @movie_1.actors << @actor_1 << @actor_2
+    @movie_2.actors << @actor_2 << @actor_3 << @actor_4
+    @movie_3.actors << @actor_1 << @actor_2 << @actor_4
   end
 
   before :each do
@@ -25,5 +34,11 @@ RSpec.describe 'The show page for a studio,' do
       expect(page).to have_content(@movie_2.title)
       expect(page).to have_content(@movie_3.title)
     end
+  end
+
+  describe 'list of actors,' do
+    it 'shows no duplicates'
+    it 'are ordered by age oldest to youngest'
+    it 'only shows currently working actors'
   end
 end
