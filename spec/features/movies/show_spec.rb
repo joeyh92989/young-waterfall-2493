d3c34db29655ba2ad3c032b3baa1a5540e4d21d9
Richard <rdesilvey@gmail.com>
2021-06-01 10:50:25 -0600

    Add feature test file

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'The show page for a movie,' do
+  it 'shows the title, creation year, and genre'
+
+  describe 'list of actors,' do
+    it 'shows all the actors in the movie'
+  end
+end
