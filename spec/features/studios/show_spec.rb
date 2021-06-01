b04a6a4a324232c19596ac561f82a3c95fe99a4f
Richard <rdesilvey@gmail.com>
2021-06-01 09:34:26 -0600

    Write feature tests

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'The show page for a studio,' do
-  it 'shows the studio name and location'
-  it 'shows the list of movie titles'
+  before :all do
+    @studio = FactoryBot.create(:studio)
+    @movie_1 = FactoryBot.create(:movie, studio: @studio)
+    @movie_2 = FactoryBot.create(:movie, studio: @studio)
+    @movie_3 = FactoryBot.create(:movie, studio: @studio)
+  end
+
+  before :each do
+    visit "/studios/#{@studio.id}"
+  end
+
+  it 'shows the studio name and location' do
+    within '#studio-stats' do
+      expect(page).to have_content(@studio.name)
+      expect(page).to have_content(@studio.location)
+    end
+  end
+
+  it 'shows the list of movie titles' do
+    within '#studio-movie-titles' do
+      expect(page).to have_content(@movie_1.title)
+      expect(page).to have_content(@movie_2.title)
+      expect(page).to have_content(@movie_3.title)
+    end
+  end
 end
