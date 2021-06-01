63e57604fefb9fdb36940557afbb5359822b4582
Richard <rdesilvey@gmail.com>
2021-06-01 10:50:38 -0600

    Add controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,12 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only %i[ show ]
+
+  def show
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+end
